e1bc6d8a835b8452a9b631dfb7439f711b7265d8
mryykt <avrpic9@gmail.com>
2024-11-25 21:07:10 +0900

    feat: kernel function for KDE

---
+++ src/Clustering/KernelFunction.elm
@@ -0,0 +1,38 @@
+module Clustering.KernelFunction exposing (..)
+
+
+gauss : Float -> Float
+gauss x =
+    1 / sqrt (2 * pi) * e ^ (-x * x / 2)
+
+
+tophat : Float -> Float
+tophat x =
+    if -1 < x && x < 1 then
+        0.5
+
+    else
+        0
+
+
+linear : Float -> Float
+linear x =
+    if abs x < 1 then
+        1 - abs x
+
+    else
+        0
+
+
+exp : Float -> Float
+exp x =
+    0.5 * e ^ -(abs x)
+
+
+epanechnikov : Float -> Float
+epanechnikov x =
+    if abs x < sqrt 5 then
+        3 / (4 * sqrt 5) * (1 - x ^ 2 / 5)
+
+    else
+        0
