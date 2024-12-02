aee719807a83fb09344976f9209f26228995fa74
mryykt <avrpic9@gmail.com>
2024-12-02 22:09:17 +0900

    refactor: split to functions

--- src/Bms/Preview.elm
+++ src/Bms/Preview.elm
@@ -20,181 +20,98 @@ view bms notess =
 
 oneMeasure : Bms -> ( Int, List Note ) -> Html msg
 oneMeasure bms ( measure, notes ) =
-    let
-        onePSide pSide notesPSide =
-            let
-                lanes =
-                    let
-                        ( minValue, maxValue ) =
-                            case bms.chartType of
-                                Key7 ->
-                                    ( 0, 7 )
-
-                                Key5 ->
-                                    ( 0, 5 )
-
-                                Key9 ->
-                                    ( 1, 9 )
-
-                                Key14 ->
-                                    ( 0, 7 )
-
-                                Key10 ->
-                                    ( 0, 5 )
-                    in
-                    (if pSide == Right then
-                        rightScratch
-
-                     else
-                        identity
-                    )
-                    <|
-                        fill minValue maxValue <|
-                            List.sortBy Tuple.first <|
-                                List.map (\( a, b ) -> ( key a.ext, a :: b )) <|
-                                    gatherEqualsBy (.ext >> key) notesPSide
-
-                quarter n =
-                    Html.div [ css [ position absolute, width (pct 100), top (pct <| n * 25), border3 (px 0.5) solid (rgb 70 70 70) ] ] []
-            in
-            Html.div
-                [ css
-                    [ position relative, width (pct 80), height (pct 100), backgroundColor (rgb 50 50 50), displayFlex ]
-                ]
-            <|
-                List.map (lane pSide bms.chartType) lanes
-                    ++ [ quarter 1, quarter 2, quarter 3 ]
-    in
     Html.div
         [ id <| "measure-" ++ String.fromInt measure
         , css
             [ position relative
             , height (pct <| 20 * (Maybe.withDefault 1.0 <| Dict.get measure bms.mlens))
             , width
-                (pct <|
-                    case bms.chartType of
-                        Key5 ->
-                            10
-
-                        Key14 ->
-                            25
-
-                        Key10 ->
-                            20
-
-                        _ ->
-                            15
-                )
+                (pct <| laneWidth bms.chartType)
             , minWidth
-                (px <|
-                    case bms.chartType of
-                        Key5 ->
-                            150
-
-                        Key14 ->
-                            300
-
-                        Key10 ->
-                            260
-
-                        _ ->
-                            220
-                )
+                (px <| laneMinWidth bms.chartType)
             , border3 (px 1) solid (rgb 255 255 255)
             , displayFlex
             , flexDirection row
             ]
         ]
     <|
-        (if bms.chartType == Key14 || bms.chartType == Key10 then
+        if bms.chartType == Key14 || bms.chartType == Key10 then
             let
                 ( left, right ) =
                     separateForDP notes
             in
-            [ onePSide Left left
-            , Html.div
-                [ css
-                    [ position relative, width (pct 10), height (pct 100), backgroundColor (rgb 100 100 100) ]
-                ]
-                []
-            , onePSide Right right
+            [ onePSide Left bms left
+            , separator
+            , onePSide Right bms right
+            , measureNumber measure
             ]
 
-         else
-            [ onePSide Left notes ]
-        )
-            ++ [ Html.div
-                    [ css
-                        [ position relative, width (pct 20), height (pct 100), backgroundColor (rgb 200 200 200) ]
-                    ]
-                    [ Html.text <| String.fromInt measure ]
-               ]
+        else
+            [ onePSide Left bms notes, measureNumber measure ]
+
+
+onePSide : PSide -> Bms -> List Note -> Html msg
+onePSide pSide bms notesPSide =
+    let
+        lanes =
+            toLanes pSide bms notesPSide
+
+        quarter n =
+            Html.div [ css [ position absolute, width (pct 100), top (pct <| n * 25), border3 (px 0.5) solid (rgb 70 70 70) ] ] []
+    in
+    Html.div
+        [ css
+            [ position relative, width (pct 80), height (pct 100), backgroundColor (rgb 50 50 50), displayFlex ]
+        ]
+    <|
+        List.map (lane pSide bms.chartType) lanes
+            ++ [ quarter 1, quarter 2, quarter 3 ]
+
+
+toLanes : PSide -> Bms -> List Note -> List ( Int, List Note )
+toLanes pSide bms notes =
+    let
+        ( minValue, maxValue ) =
+            laneRange bms.chartType
+    in
+    gatherEqualsBy (.ext >> key) notes
+        |> List.map (\( a, b ) -> ( key a.ext, a :: b ))
+        |> List.sortBy Tuple.first
+        |> fill minValue maxValue
+        |> (if pSide == Right then
+                rightScratch
+
+            else
+                identity
+           )
+
+
+separator : Html msg
+separator =
+    Html.div
+        [ css
+            [ position relative, width (pct 10), height (pct 100), backgroundColor (rgb 100 100 100) ]
+        ]
+        []
+
+
+measureNumber : Int -> Html msg
+measureNumber n =
+    Html.div
+        [ css
+            [ position relative, width (pct 20), height (pct 100), backgroundColor (rgb 200 200 200) ]
+        ]
+        [ Html.text <| String.fromInt n ]
 
 
 lane : PSide -> ChartType -> ( Int, List Note ) -> Html msg
 lane pSide chartType ( k, notes ) =
     let
         w =
-            case chartType of
-                Key9 ->
-                    100 / 9
-
-                Key7 ->
-                    if k == 0 then
-                        16
-
-                    else
-                        12
-
-                Key5 ->
-                    if k == 0 then
-                        20
-
-                    else
-                        15
-
-                Key14 ->
-                    if k == 0 then
-                        16
-
-                    else
-                        12
-
-                Key10 ->
-                    if k == 0 then
-                        20
-
-                    else
-                        15
+            noteWidth chartType k
 
         c =
-            case chartType of
-                Key9 ->
-                    if k == 1 || k == 9 then
-                        rgb 255 255 255
-
-                    else if k == 2 || k == 8 then
-                        rgb 255 255 0
-
-                    else if k == 3 || k == 7 then
-                        rgb 0 255 0
-
-                    else if k == 4 || k == 6 then
-                        rgb 0 0 255
-
-                    else
-                        rgb 255 0 0
-
-                _ ->
-                    -- 7key, 5key, 14key, 10key
-                    if k == 0 then
-                        rgb 255 0 0
-
-                    else if modBy 2 k == 1 then
-                        rgb 255 255 255
-
-                    else
-                        rgb 0 100 255
+            noteColor chartType k
 
         note n =
             Html.div
@@ -280,3 +197,120 @@ fill minValue maxValue xs =
 
             else
                 ( minValue, [] ) :: fill (minValue + 1) maxValue xs
+
+
+laneRange : ChartType -> ( Int, Int )
+laneRange chartType =
+    case chartType of
+        Key7 ->
+            ( 0, 7 )
+
+        Key5 ->
+            ( 0, 5 )
+
+        Key9 ->
+            ( 1, 9 )
+
+        Key14 ->
+            ( 0, 7 )
+
+        Key10 ->
+            ( 0, 5 )
+
+
+laneWidth : ChartType -> Float
+laneWidth chartType =
+    case chartType of
+        Key5 ->
+            10
+
+        Key14 ->
+            25
+
+        Key10 ->
+            20
+
+        _ ->
+            15
+
+
+laneMinWidth : ChartType -> Float
+laneMinWidth chartType =
+    case chartType of
+        Key5 ->
+            150
+
+        Key14 ->
+            300
+
+        Key10 ->
+            260
+
+        _ ->
+            220
+
+
+noteWidth : ChartType -> Int -> Float
+noteWidth chartType k =
+    case chartType of
+        Key9 ->
+            100 / 9
+
+        Key7 ->
+            if k == 0 then
+                16
+
+            else
+                12
+
+        Key5 ->
+            if k == 0 then
+                20
+
+            else
+                15
+
+        Key14 ->
+            if k == 0 then
+                16
+
+            else
+                12
+
+        Key10 ->
+            if k == 0 then
+                20
+
+            else
+                15
+
+
+noteColor : ChartType -> Int -> Color
+noteColor chartType k =
+    case chartType of
+        Key9 ->
+            if k == 1 || k == 9 then
+                rgb 255 255 255
+
+            else if k == 2 || k == 8 then
+                rgb 255 255 0
+
+            else if k == 3 || k == 7 then
+                rgb 0 255 0
+
+            else if k == 4 || k == 6 then
+                rgb 0 0 255
+
+            else
+                rgb 255 0 0
+
+        _ ->
+            -- 7key, 5key, 14key, 10key
+            if k == 0 then
+                rgb 255 0 0
+
+            else if modBy 2 k == 1 then
+                rgb 255 255 255
+
+            else
+                rgb 0 100 255
