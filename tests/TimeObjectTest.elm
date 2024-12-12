99cc26591362c3ba8fccb67e3b60a31e0bc8c7ed
mryykt <avrpic9@gmail.com>
2024-12-12 15:47:54 +0900

    fix: test

--- tests/TimeObjectTest.elm
+++ tests/TimeObjectTest.elm
@@ -12,8 +12,8 @@ timeObjectTest =
         [ test "to time test-1" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.fromMeasureAndFraction (Array.fromList [ 0, 960, 1920 ]) 1 0.5) 1440
         , test "to time test-2" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.fromMeasureAndFraction (Array.fromList [ 0, 960, 1440 ]) 1 0.5) 1200
         , test "to time test-3" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.fromMeasureAndFraction (Array.fromList [ 0, 960, 1440 ]) 2 0.5) 1920
-        , test "from time test-1" <| \_ -> Expect.equal (TimeObject.getFraction (Array.fromList [ 0, 960, 1920 ]) { time = 1440, measure = 1 }) 0.5
-        , test "from time test-2" <| \_ -> Expect.equal (TimeObject.getFraction (Array.fromList [ 0, 960, 1440 ]) { time = 1200, measure = 1 }) 0.5
-        , test "from time test-3" <| \_ -> Expect.equal (TimeObject.getFraction (Array.fromList [ 0, 960, 1920 ]) { time = 480, measure = 0 }) 0.5
-        , test "from time test-4" <| \_ -> Expect.equal (TimeObject.getFraction (Array.fromList [ 0, 960, 1920 ]) { time = 2400, measure = 2 }) 0.5
+        , test "from time test-1" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.getFraction (Array.fromList [ 0, 960, 1920 ]) { time = 1440, measure = 1 }) 0.5
+        , test "from time test-2" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.getFraction (Array.fromList [ 0, 960, 1440 ]) { time = 1200, measure = 1 }) 0.5
+        , test "from time test-3" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.getFraction (Array.fromList [ 0, 960, 1920 ]) { time = 480, measure = 0 }) 0.5
+        , test "from time test-4" <| \_ -> Expect.within (Expect.Absolute 0.001) (TimeObject.getFraction (Array.fromList [ 0, 960, 1920 ]) { time = 2400, measure = 2 }) 0.5
         ]
