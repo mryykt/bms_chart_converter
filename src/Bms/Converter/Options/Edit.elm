3f8d75b0bdb25180cad05231afbdfafbb0ec6e2f
mryykt <avrpic9@gmail.com>
2025-01-02 15:05:02 +0900

    feat: Formにdisbaledにするオプション

--- src/Bms/Converter/Options/Edit.elm
+++ src/Bms/Converter/Options/Edit.elm
@@ -18,6 +18,15 @@ type Msg a
     | Update (a -> a)
 
 
+type alias FormOptions =
+    { disbaled : Bool }
+
+
+defOptions : FormOptions
+defOptions =
+    { disbaled = False }
+
+
 update : Msg Form -> Form -> Form
 update msg form =
     case msg of
@@ -31,12 +40,14 @@ update msg form =
 view : Form -> Html (Msg Form)
 view form =
     Html.div []
-        [ select "chart type"
+        [ select { defOptions | disbaled = True }
+            "chart type"
             (Dict.fromList [ ( "7-key", Key7 ), ( "5-key", Key5 ), ( "9-key", Key9 ), ( "14-key", Key14 ), ( "10-key", Key10 ) ])
             chartType
             form
-        , text "number" "band width" bandWidth form
-        , select "kernel function"
+        , text "number" defOptions "band width" bandWidth form
+        , select defOptions
+            "kernel function"
             (Dict.fromList
                 [ ( "Gauss", Kernel.Gauss )
                 , ( "Exponetail", Kernel.Exp )
@@ -47,20 +58,21 @@ view form =
             )
             kernelFunction
             form
-        , bool "increase scratch" { getter = .increaseScratchOptions, setter = \x y -> { y | increaseScratchOptions = x } } form
+        , bool { defOptions | disbaled = form.chartType.value == Key9 } "increase scratch" { getter = .increaseScratchOptions, setter = \x y -> { y | increaseScratchOptions = x } } form
         , Html.fieldset [ Attributes.disabled <| not <| .value <| .increaseScratchOptions form ]
-            [ text "number" "minimum duration" minDuration form
-            , bool "include long-note" isIncludeLn form
+            [ text "number" defOptions "minimum duration" minDuration form
+            , bool defOptions "include long-note" isIncludeLn form
             ]
         ]
 
 
-text : String -> String -> Lens a (Field String) -> a -> Html (Msg a)
-text typ label lens v =
+text : String -> FormOptions -> String -> Lens a (Field String) -> a -> Html (Msg a)
+text typ opt label lens v =
     field label <|
         Html.input
             [ class "input"
             , whenAttribute (.invalid <| Lens.get lens v) <| class "is-danger"
+            , Attributes.disabled opt.disbaled
             , Attributes.type_ typ
             , onInput (Update << (Lens.compose value lens).setter)
             , Attributes.value (.value <| lens.getter v)
@@ -68,8 +80,8 @@ text typ label lens v =
             []
 
 
-bool : String -> Lens a (Field Bool) -> a -> Html (Msg a)
-bool label lens v =
+bool : FormOptions -> String -> Lens a (Field Bool) -> a -> Html (Msg a)
+bool opt label lens v =
     let
         lens_ =
             Lens.compose value lens
@@ -78,6 +90,7 @@ bool label lens v =
         Html.label [ class "checkbox" ]
             [ Html.input
                 [ Attributes.type_ "checkbox"
+                , Attributes.disabled opt.disbaled
                 , onCheck (Update << lens_.setter)
                 , Attributes.checked (Lens.get lens_ v)
                 ]
@@ -85,8 +98,8 @@ bool label lens v =
             ]
 
 
-select : String -> Dict String b -> Lens a (Field b) -> a -> Html (Msg a)
-select label dict lens v =
+select : FormOptions -> String -> Dict String b -> Lens a (Field b) -> a -> Html (Msg a)
+select opt label dict lens v =
     let
         option val =
             Html.option
@@ -106,6 +119,7 @@ select label dict lens v =
         Html.div [ class "select" ]
             [ Html.select
                 [ onInput (Update << lens_.setter << get)
+                , Attributes.disabled opt.disbaled
                 ]
               <|
                 List.map option <|
